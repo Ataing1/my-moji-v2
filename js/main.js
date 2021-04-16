c02a03b96804a41d18268158d5f65b4e7a283adb
Ataing1 <56658290+Ataing1@users.noreply.github.com>
2021-04-15 22:46:39 -0700

    added .htaccess, implemented dynamoDB GET and UPDATE
    .htacess - to hide .html extension in url.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
-function buttonClick() {
-	alert("button clicked");
-}
+let testButton = document.getElementById("section3button");
+
+let formData = new FormData();//build form data based on artist or feedback page, then use that as body for post call
+formData.append("renditions", null);
+formData.append("feedback", "new peice of feedback");
+testButton.addEventListener('click', e =>{
+	fetch("/testing/abc123", {
+		method: 'POST',
+		body: formData
+	}).then(r => r.json());
+})
--- server.js
+++ server.js
@@ -6,15 +6,23 @@ const multer = require('multer');
 const upload = multer();
 const url = require('url');
 const {S3Client, PutObjectCommand, GetObjectCommand} = require("@aws-sdk/client-s3");
-const { getSignedUrl  } = require("@aws-sdk/s3-request-presigner");
+const {
+	DynamoDBClient,
+	PutItemCommand,
+	GetItemCommand,
+	UpdateItemCommand,
+} = require("@aws-sdk/client-dynamodb");
+const {marshall, unmarshall} = require("@aws-sdk/util-dynamodb");
+const {getSignedUrl} = require("@aws-sdk/s3-request-presigner");
+
+
 const fs = require('fs');
 const REGION = "us-east-2"; // Set the AWS Region. e.g. "us-east-1"
-const { v4: uuidV4 } = require('uuid')
-
-
+const {v4: uuidV4} = require('uuid')
+const devMode = process.env.NODE_ENV === 'development';
 
 //development mode uses DOTENV to load a .env file which contains the environmental variables. Access via process.env
-if (process.env.NODE_ENV === 'development') {
+if (devMode) {
 	console.log("THIS IS DEVELOPMENT MODE");
 	console.log(require('dotenv').config())
 	require('dotenv').config({path: './.env'});
@@ -66,29 +74,58 @@ app.get('/checkout-session', async (req, res) => {
 	res.send(session);
 });
 
-app.post('/create-checkout-session/:uuid', async (req, res) => {
+app.post('/create-checkout-session', upload.single('upload'), async (req, res) => {
 	console.log("create checkout session called");
+	console.log("req.body", req.body);
+	console.log("req.file", req.file);
+
+	const uuid = devMode ? "abc123" : uuidV4(); //if devmode: hard coded value, else a unique ID
 	const domainURL = process.env.DOMAIN;
 	const pmTypes = (process.env.PAYMENT_METHOD_TYPES || 'card').split(',').map((m) => m.trim());
 	const session = await stripe.checkout.sessions.create({
+		client_reference_id: req.body.uuid,
+		customer_email: req.body.email,
 		payment_method_types: pmTypes,
 		mode: 'payment',
-		locale: req.body.locale,
 		line_items: [
 			{
 				price: process.env.PRICE,
 				quantity: 1
 			},
 		],
+		metadata: {
+			name: req.body.name,
+			email: req.body.email,
+			uuid: req.body.uuid,
+		},
+
 		// ?session_id={CHECKOUT_SESSION_ID} means the redirect will have the session ID set as a query param
-		success_url: `${domainURL}/success.html?session_id={CHECKOUT_SESSION_ID}&uuid=`+req.params.uuid,
+		success_url: `${domainURL}/success.html?session_id={CHECKOUT_SESSION_ID}&uuid=` + uuid,
 		cancel_url: `${domainURL}/newOrder.html`,
 	});
+	await uploadImageToS3(req.file, uuid);
+	//add sessionID to database object and add object to database.
+	//add date created with date.now
+	let newOrder = {
+		customer_id: uuid,
+		name: req.body.name,
+		email: req.body.email,
+		notes: req.body.notes,
+		session_id: session.id,
+		created_at: new Date(Date.now()).toString(),
+		updated_at: new Date(Date.now()).toString(),
+		status: "pending-rendition",
+		feedback: [],
+		renditions: 0
+
+	}
+	await putItemInDatabase(newOrder);
 
 	res.send({
 		sessionId: session.id,
 	});
 });
+
 
 // Webhook handler for asynchronous events.
 app.post('/webhook', async (req, res) => {
@@ -133,24 +170,24 @@ app.post('/webhook', async (req, res) => {
 	res.sendStatus(200);
 });
 
-app.get('/uuid', async (request, response)=>{
-	response.send({uuid: uuidV4()})
-})
 
-app.post('/form-data', upload.single('upload'), async (req, res) => {
-	console.log("req.file", req.file);
-	console.log("req.body", req.body);
-	let {name, email, uuid, sessionID} = req.body;
-	console.log(name, email, uuid, sessionID);
-	//upload user data to databse of some sort
-	await uploadImageToS3(req.file, uuid);
-
-});
-
+/**
+ * gets image url from s3 database
+ * uuid/type.png
+ * type - "INITAL UPLOAD, RENDITION, ..."
+ */
 app.get('/photo/:uuid/:type', async (req, res) => {
 	console.log("uuid: ", req.params.uuid);
 	console.log("uuid: ", req.params.type);
 	const data = await getImageUrlFromS3(req.params.uuid, req.params.type);
+	res.send(data);
+})
+
+app.post('/testing/:uuid',upload.none(), async (req, res) => {
+	//loop through req.body, and set it to map
+	console.log("testing called");
+	// {renditions: 1, feedback: ["this is another one"]}
+	const data = await updateItemInDatabase(req.params.uuid,req.body);
 	res.send(data);
 })
 
@@ -190,14 +227,120 @@ function checkEnv() {
 	}
 }
 
-async function getImageUrlFromS3(uuid, type){
+//let let contacts = new Map()
+// contacts.set('Jessie', {phone: "213-555-1234", address: "123 N 1st Ave"})
+// contacts.has('Jessie') // true
+// contacts.get('Hilary') // undefined
+// contacts.set('Hilary', {phone: "617-555-4321", address: "321 S 2nd St"})
+// contacts.get('Jessie') // {phone: "213-555-1234", address: "123 N 1st Ave"}
+// contacts.delete('Raymond') // false
+// contacts.delete('Jessie') // true
+// console.log(contacts.size) // 1
+/**
+ *
+ * @param uuid customer_id
+ * @param items map of keys that need updating
+ * @returns {Promise<void>}
+ */
+async function updateItemInDatabase(uuid, items) {
+	if (items.size === 0) return; //do nothing if map is empty
+	let updateExpression = "SET ";
+	let updateValues = {}
+	// for (const [key, value] of items) {
+	// 	console.log("key", key, "value", value);
+	// 	switch (key) {
+	// 		case "renditions":
+	// 			updateExpression += "renditions = renditions + :renditions, ";
+	// 			updateValues[":renditions"] = value;
+	// 			break;
+	// 		case "feedback":
+	// 			updateExpression += "feedback = list_append(feedback, :feedback), ";
+	// 			updateValues[":feedback"] = value;
+	// 			break;
+	// 	}
+	// }
+	console.log("items", items);
+	const keys = Object.keys(items);
+	for(let i = 0; i < keys.length; i++){
+		//inserting new key value pair inside map
+		// map.set(keys[i], obj[keys[i]]);
+		switch (keys[i]) {
+			case "renditions":
+				updateExpression += "renditions = renditions + :renditions, ";
+				updateValues[":renditions"] = 1;
+				break;
+			case "feedback":
+				updateExpression += "feedback = list_append(feedback, :feedback), ";
+				updateValues[":feedback"] = [items[keys[i]]]; //turns value in items[key[i]] into a list
+				break;
+		}
+	};
+
+	updateExpression += "updated_at = :update_time";
+	updateValues[":update_time"] =	new Date(Date.now()).toString();
+
+	console.log("update expression: ", updateExpression);
+	console.log("update values: ", updateValues);
+	//EXAMPLE OF PARAMS WHERE YOU INCREMENT RENDITIONS, APPEND A STRING TO A LIST, AND UPDATE A VALUE
+	// const params = {
+	// 	TableName: "orders",
+	// 	Key: marshall({
+	// 		customer_id: uuid
+	// 	}),
+	// 	UpdateExpression: "SET renditions = renditions + :renditions, feedback = list_append(feedback, :feedback), updated_at = :update_time",
+	// 	ExpressionAttributeValues: marshall({
+	// 		":renditions": 1,
+	// 		":feedback": ["new feedback"],
+	// 		":update_time": new Date(Date.now()).toString(),
+	// 	})
+	// };
+
+	const params = {
+		TableName: "orders",
+		Key: marshall({
+			customer_id: uuid
+		}),
+		UpdateExpression: updateExpression,
+		ExpressionAttributeValues: marshall(updateValues)
+	};
+
+	const client = new DynamoDBClient({region: REGION});
+
+	try {
+		const data = await client.send(new UpdateItemCommand(params));
+		console.log("Success - updated", data);
+	} catch (err) {
+		console.log("Error", err);
+	}
+}
+
+async function putItemInDatabase(data) {
+	// Set the parameters
+	const params = {
+		TableName: "orders",
+		Item: marshall(data),
+	};
+
+// Create DynamoDB service object
+	const dbclient = new DynamoDBClient({region: REGION});
+
+	try {
+		const data = await dbclient.send(new PutItemCommand(params));
+		console.log("success");
+		console.log(data);
+	} catch (err) {
+		console.error(err);
+	}
+}
+
+async function getImageUrlFromS3(uuid, type) {
 	let imageParams = {
 		Bucket: "mymojibucket",
 		Key: "",
 	}
-	if (type === "INITIAL_UPLOAD"){
+	if (type === "INITIAL_UPLOAD") {
 		imageParams.Key = uuid + "/initialUpload.png";
-	}else if(type === "RENDITION"){
+	} else if (type === "RENDITION") {
 		imageParams.Key = uuid + ""; //insert some other path to the rendition photo
 	}
 
@@ -215,16 +358,12 @@ async function getImageUrlFromS3(uuid, type){
 		console.log(signedUrl);
 
 		return ({"signed": signedUrl, "potatoe": "i spelled that wrong"});
-	}
-	catch (err) {
+	} catch (err) {
 		console.log("Error creating presigned URL", err);
 	}
 }
 
 async function uploadImageToS3(file, uuid) {
-
-	console.log("enter form upload");
-
 	let imageParams = {
 		Bucket: "mymojibucket",
 		Key: uuid + "/initialUpload.png",
@@ -237,8 +376,8 @@ async function uploadImageToS3(file, uuid) {
 	// Create an Amazon S3 service client object.
 	const s3 = new S3Client({region: REGION});
 	try {
-		const data = await s3.send(new PutObjectCommand(imageParams));
-		// console.log("Success", data);
+		await s3.send(new PutObjectCommand(imageParams));
+		console.log("Image uploaded Successfully");
 	} catch (err) {
 		console.log("Error", err);
 
