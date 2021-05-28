3d2db0dbf49663c4ca710d438b7b5b23b098b555
Ataing1 <56658290+Ataing1@users.noreply.github.com>
2021-05-27 17:40:40 -0700

    improved header and footer styling. header: added dropdown menu when the screen gets too small footer: added facebook icon and made center links always centered

--- js/main.js
+++ js/main.js
@@ -50,16 +50,23 @@ function validateFileUpload() {
 	return valid;
 }
 
-async function getURL(uuid, name){
-
-	fetch("/photo/"+ uuid+ "/" + name)
-		.then((response) => response.json())
-		.then((data) => {
-			return data.signed;
-		})
-		.catch((error) => {
-			console.error("Error: ", error);
-		});
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function showDropDown() {
+	document.getElementById("myDropdown").classList.toggle("show");
 }
 
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+	if (!event.target.matches('.dropbtn')) {
+		var dropdowns = document.getElementsByClassName("dropdown-content");
+		var i;
+		for (i = 0; i < dropdowns.length; i++) {
+			var openDropdown = dropdowns[i];
+			if (openDropdown.classList.contains('show')) {
+				openDropdown.classList.remove('show');
+			}
+		}
+	}
+}
 
